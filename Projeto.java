a862dd3cabda541b8c71195ee6d950e1fb35d339
pedroboccatoalmeida-lang <pedro.boccatoalmeida@gmail.com>
2025-09-22 19:42:41 -0300

    Create Projeto.java

---
+++ Projeto.java
@@ -0,0 +1,10 @@
+public class Projeto {
+    private String nome;
+    private String descricao;
+    private String dataInicio;
+    private String dataFimPrevisto;
+    private String status;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
