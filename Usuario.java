232050e9f171650aeed796fe2b634a8a4843ca40
pedroboccatoalmeida-lang <pedro.boccatoalmeida@gmail.com>
2025-09-22 19:40:43 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String cargo;
+    private String login;
+    private String senha;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
